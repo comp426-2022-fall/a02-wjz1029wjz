2ffb8adb16fa215650fa3d436625654bf5b43be9
Jiazhen Wu <97967159+wjz1029wjz@users.noreply.github.com>
2022-11-28 09:56:40 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -7,14 +7,14 @@ import moment from "moment-timezone";
 const args = minimist(process.argv.slice(2))
 
 if(args.h) {
-	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    -h            Show this help message and exit.
-    -n, -s        Latitude: N positive; S negative.
-    -e, -w        Longitude: E positive; W negative.
-    -z            Time zone: uses tz.guess() from moment-timezone by default.
-    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.`);
-	process.exit(0);
+	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE');
+    	console.log('-h            Show this help message and exit.');
+    	console.log('-n, -s        Latitude: N positive; S negative.');
+    	console.log('-e, -w        Longitude: E positive; W negative.');
+    	console.log('-z            Time zone: uses tz.guess() from moment-timezone by default.');
+    	console.log('-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.');
+    	console.log('-j            Echo pretty JSON from open-meteo API and exit.');
+    	process.exit(0);
 }
 
 var timezone = moment.tz.guess();
@@ -24,7 +24,7 @@ var longitude = args.e || args.w * -1;
 
 
 
-if (args.z) {
+/*if (args.z) {
     timezone = args.z;
 }
 
@@ -46,7 +46,7 @@ else if (args.w){
 }
 else{
   longitude = 0;
-}
+}*/
 
 const response = await nodefetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=' + timezone);
 const data = await response.json();
